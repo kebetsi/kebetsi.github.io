ae9c0dacc7216e6cbe3e830949b9ba54a0663f88
Ilia Kebets <kebetsi@gmail.com>
2024-10-02 08:10:15 +0200

    change url to point to right repo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -67,7 +67,7 @@ const config: Config = {
       items: [
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/kebetsi/kebetsi.github.io',
           label: 'GitHub',
           position: 'right',
         },
