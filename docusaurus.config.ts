42143b9fa0dcfb1d8a8886056c75f98ec8d71f8a
Ilia Kebets <kebetsi@gmail.com>
2024-10-02 08:08:48 +0200

    remove sidebar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -33,13 +33,6 @@ const config: Config = {
     [
       'classic',
       {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
         blog: {
           showReadingTime: true,
           feedOptions: {
@@ -72,12 +65,6 @@ const config: Config = {
         src: 'img/logo.svg',
       },
       items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
-        },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/facebook/docusaurus',
