ae11675d6f6e529c9cbdd5df05dec27c8c208918
kebetsi <ilia.kebets@sonarsource.com>
2024-11-18 07:37:21 +0000

    add inter-process sync article

--- assets/js/c15d9823.9182d7fe.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkilia_kebets_tech_blog=self.webpackChunkilia_kebets_tech_blog||[]).push([[146],{9328:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
